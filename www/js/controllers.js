1e661e156bb84ed246dec94b682a797576c41a3d
Dmitri Skjorshammer <dmitriskj@gmail.com>
2016-03-10 22:09:21 -0800

    Ports Ionic Creator over to Ionic Framework.

---
+++ www/js/controllers.js
@@ -0,0 +1,42 @@
+angular.module('app.controllers', [])
+     
+.controller('loginCtrl', function($scope) {
+
+})
+   
+.controller('measurementsCtrl', function($scope) {
+
+})
+   
+.controller('appointmentsCtrl', function($scope) {
+
+})
+   
+.controller('goalsCtrl', function($scope) {
+
+})
+   
+.controller('addGoalCtrl', function($scope) {
+
+})
+   
+.controller('appointmentCtrl', function($scope) {
+
+})
+   
+.controller('symptomsSliderCtrl', function($scope) {
+
+})
+   
+.controller('symptomsListCtrl', function($scope) {
+
+})
+   
+.controller('symptomsListMultipleCtrl', function($scope) {
+
+})
+   
+.controller('measurementTipsCtrl', function($scope) {
+
+})
+ 
