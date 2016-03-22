4b8b12a15920e2f5830f0584c748f5254a295eef
Dmitri Skjorshammer <dmitriskj@gmail.com>
2016-03-21 23:32:43 -0700

    Clean up Comment controller.

--- www/js/controllers/commentsCtrl.js
+++ www/js/controllers/commentsCtrl.js
@@ -1,16 +1,14 @@
 angular.module('app.controllers')
 
 .controller('commentsCtrl', function($scope, $stateParams, Comment) {
-  $scope.comments = Comment.getByCardId($stateParams.id);
+  $scope.comments   = Comment.getByCardId($stateParams.id);
+  $scope.newComment = {card_id: $stateParams.id, user_name: "Mr. A"}
 
-  $scope.reply = function(replyContent){
-  	new_comment = {};
-  	new_comment.cardId = $stateParams.id;
-  	new_comment.user_name = "Mr.A";
-  	new_comment.content = replyContent;
-  	Comment.add(new_comment);
-  	$scope.comments = Comment.getByCardId($stateParams.id);
-  	$scope.replyContent = "";
+  $scope.reply = function(){
+    // NOTE: We copy newComment in order to avoid resetting it after the fact.
+  	Comment.add(angular.copy($scope.newComment));
+    $scope.comments = Comment.getByCardId($stateParams.id);
+    $scope.newComment.content = "";
   }
 
 })
