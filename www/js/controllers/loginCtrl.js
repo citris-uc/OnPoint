1527ff3c777835da6bc335b9d259e7495fa57935
Dmitri Skjorshammer <dmitriskj@gmail.com>
2016-03-29 11:18:07 -0700

    Updates loginCtrl.js to use Patient factory.

--- www/js/controllers/loginCtrl.js
+++ www/js/controllers/loginCtrl.js
@@ -1,45 +1,40 @@
 angular.module('app.controllers')
 
-.controller('loginCtrl', function($scope, $state, $stateParams, $ionicPopup) {
+.controller('loginCtrl', function($scope, $state, Auth, Patient, $stateParams, $ionicPopup) {
   $scope.user  = {email: 'ucb.onpoint@gmail.com', password: 'onpoint'};
   $scope.state = {loading: false}
 
-  $scope.logIn = function(){
+  $scope.login = function(){
     $scope.state.loading = true;
 
-    var firebaseRef = new Firebase("https://vivid-inferno-5187.firebaseio.com/");
-    firebaseRef.authWithPassword($scope.user, function(error, authData) {
-      if (error) {
-        var alertPopup = $ionicPopup.alert({
-          title: 'Error',
-          template: error
-        });
-      } else {
-        window.localStorage.setItem("authData", JSON.stringify(authData));
-        $state.go("tabsController.timeline");
-      }
-
+    Auth.$authWithPassword($scope.user).then(function(authData) {
       $scope.state.loading = false;
-    });
+      Patient.setToken(authData.token)
+      $state.go("tabsController.timeline");
+    }).catch(function(error) {
+      handleError(error)
+    })
+
   }
 
-  $scope.createAccount = function()   {
+  $scope.register = function()   {
     $scope.state.loading = true;
 
-    var firebaseRef = new Firebase("https://vivid-inferno-5187.firebaseio.com/");
-    firebaseRef.createUser($scope.user, function(error, authData) {
-      if (error) {
-        var alertPopup = $ionicPopup.alert({
-          title: 'Error creating user',
-          template: error
-        });
-      } else {
-        window.localStorage.setItem("authData", JSON.stringify(authData));
-        $state.go("tabsController.timeline");
-      }
-
+    Auth.$createUser($scope.user).then(function(authData) {
       $scope.state.loading = false;
+      Patient.create($scope.user.email, authData)
+      $state.go("tabsController.timeline");
+    }).catch(function(error) {
+      handleError(error)
+    })
+  }
+
+  var handleError = function(error) {
+    var alertPopup = $ionicPopup.alert({
+      title: 'Error',
+      template: error
     });
+    $scope.state.loading = false;
   }
 
 })
