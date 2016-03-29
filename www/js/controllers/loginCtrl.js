a37fc5cbd7ae02d7b14faa23da65c63e5e2ba5c8
Dmitri Skjorshammer <dmitriskj@gmail.com>
2016-03-29 09:57:10 -0700

    Cleans up loginCtrl.

--- www/js/controllers/loginCtrl.js
+++ www/js/controllers/loginCtrl.js
@@ -1,45 +1,44 @@
 angular.module('app.controllers')
 
-.controller('loginCtrl', function($scope, $state, $stateParams,$ionicPopup) {
-  $scope.user ={email: 'ucb.onpoint@gmail.com', password: 'onpoint'};
+.controller('loginCtrl', function($scope, $state, $stateParams, $ionicPopup) {
+  $scope.user  = {email: 'ucb.onpoint@gmail.com', password: 'onpoint'};
+  $scope.state = {loading: false}
 
   $scope.logIn = function(){
+    $scope.state.loading = true;
+
     var firebaseRef = new Firebase("https://vivid-inferno-5187.firebaseio.com/");
-    firebaseRef.authWithPassword({
-      email : $scope.user.email,
-      password: $scope.user.password
-    }, function(error, authData) {
+    firebaseRef.authWithPassword($scope.user, function(error, authData) {
       if (error) {
-      console.log($scope.user.email);
-      console.log($scope.user.password);
-      console.log("Login Failed!", error)
+        var alertPopup = $ionicPopup.alert({
+          title: 'Error',
+          template: error
+        });
+      } else {
+        window.localStorage.setItem("authData", JSON.stringify(authData));
+        $state.go("tabsController.timeline");
+      }
 
-    var alertPopup = $ionicPopup.alert({
-      title: 'Error',
-      template: error
+      $scope.state.loading = false;
     });
-
-    } else {
-      console.log("Authenticated successfully with payload:", authData);
-      window.localStorage["authData"] = JSON.stringify(authData);
-      console.log(JSON.parse(window.localStorage["authData"])); 
-      //$localstorage.setObject("authData", authData);
-      $state.go("tabsController.timeline");
-    }
-  });
   }
 
   $scope.createAccount = function()   {
+    $scope.state.loading = true;
+
     var firebaseRef = new Firebase("https://vivid-inferno-5187.firebaseio.com/");
-    firebaseRef.createUser({
-      email : $scope.user.email,
-      password: $scope.user.password
-    }, function(error, userData) {
+    firebaseRef.createUser($scope.user, function(error, authData) {
       if (error) {
-        console.log("Error creating user:", error);
+        var alertPopup = $ionicPopup.alert({
+          title: 'Error creating user',
+          template: error
+        });
       } else {
-        console.log("Successfully created user account with uid:", userData.uid);
+        window.localStorage.setItem("authData", JSON.stringify(authData));
+        $state.go("tabsController.timeline");
       }
+
+      $scope.state.loading = false;
     });
   }
 
