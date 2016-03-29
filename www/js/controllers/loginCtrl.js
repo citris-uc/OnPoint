0431e45782d8523b00938bc997e5580eea05bf35
Dmitri Skjorshammer <dmitriskj@gmail.com>
2016-03-29 11:29:21 -0700

    Ensure that next view is the history root and can't navigate back

--- www/js/controllers/loginCtrl.js
+++ www/js/controllers/loginCtrl.js
@@ -1,6 +1,6 @@
 angular.module('app.controllers')
 
-.controller('loginCtrl', function($scope, $state, Auth, Patient, $stateParams, $ionicPopup) {
+.controller('loginCtrl', function($scope, $state, $ionicHistory, Auth, Patient, $ionicPopup) {
   $scope.user  = {email: 'ucb.onpoint@gmail.com', password: 'onpoint'};
   $scope.state = {loading: false}
 
@@ -8,24 +8,33 @@ angular.module('app.controllers')
     $scope.state.loading = true;
 
     Auth.$authWithPassword($scope.user).then(function(authData) {
-      $scope.state.loading = false;
-      Patient.setToken(authData.token)
+      handleTransition()
+      Patient.setToken(authData.token);
       $state.go("tabsController.timeline");
     }).catch(function(error) {
       handleError(error)
     })
-
   }
 
   $scope.register = function()   {
     $scope.state.loading = true;
 
     Auth.$createUser($scope.user).then(function(authData) {
-      $scope.state.loading = false;
+      handleTransition()
       Patient.create($scope.user.email, authData)
       $state.go("tabsController.timeline");
     }).catch(function(error) {
       handleError(error)
+    })
+  }
+
+  var handleTransition = function() {
+    $scope.state.loading = false;
+
+    $ionicHistory.nextViewOptions({
+      disableAnimate: true,
+      disableBack: true,
+      historyRoot: true
     })
   }
 
