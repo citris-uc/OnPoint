a851c9e15b570128721b8d9a1bdaa575ccdbb2f0
Dmitri Skjorshammer <dmitriskj@gmail.com>
2016-03-29 23:56:39 -0700

    Refactors Patient services to add new convenience methods.

--- www/js/services/patient.js
+++ www/js/services/patient.js
@@ -6,8 +6,11 @@ is returned via password authentication:
 https://www.firebase.com/docs/web/guide/login/password.html
 */
 angular.module('app.services')
-.factory('Patient', function($window) {
+.factory('Patient', function($window, $firebaseAuth) {
   return {
+    uid: function() {
+      return this.get().uid;
+    },
     get: function() {
       return JSON.parse($window.localStorage.getItem("patient") || "{}");
     },
@@ -18,7 +21,7 @@ angular.module('app.services')
       // Create the patient, add to localstorage, and add the token.
       patient = {email: email, uid: authData.uid, profileImageUrl: authData.profileImageUrl}
       this.set(patient)
-      this.ref().set({email: email})
+      this.ref(authData.uid).set({email: email})
       this.setToken(authData.token)
     },
     setProfilePicture: function(profileImageUrl) {
@@ -27,13 +30,27 @@ angular.module('app.services')
       this.set(patient)
     },
     setToken: function(token) {
-      $window.localStorage.setItem("token", token);
+      if (token == null)
+        $window.localStorage.removeItem("token");
+      else
+        $window.localStorage.setItem("token", token);
     },
     getToken: function() {
       $window.localStorage.getItem("token");
     },
-    ref: function() {
-      return new Firebase("https://vivid-inferno-5187.firebaseio.com/patients/").child(this.get().uid);
+    ref: function(uid) {
+      var patientRef = new Firebase("https://vivid-inferno-5187.firebaseio.com/patients/");
+      if (uid)
+        return patientRef.child(uid);
+      else
+        return patientRef;
+    },
+    auth: function() {
+      return $firebaseAuth(this.ref());
+    },
+    logout: function() {
+      this.auth().$unauth();
+      this.setToken(null);
     }
   };
 })
