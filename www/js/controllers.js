c81b881c35d5748b7c39289e626b403d101e02df
Dmitri Skjorshammer <dmitriskj@gmail.com>
2016-03-29 23:56:07 -0700

    Adds ability to logout via Settings.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -142,3 +142,15 @@ $scope.bpAlert = function(value) {
 .controller('symptomsListMultipleCtrl', function($scope) {
 
 })
+
+.controller('settingsCtrl', function($scope, Patient, $state, $ionicHistory) {
+  $scope.logout = function() {
+    Patient.logout();
+    $ionicHistory.nextViewOptions({
+      disableAnimate: true,
+      disableBack: true,
+      historyRoot: true
+    })
+    $state.go("loginScreen");
+  }
+})
