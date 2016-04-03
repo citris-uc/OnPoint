dbec46b55ebd9ca86cb056457803ec4fd9906faa
zhuosi <wangzhuosi13@hotmail.com>
2016-04-03 13:48:32 -0700

    tried copy past index.html, timelineCtrl.js and timeline.html file completely from master. But the console has an error of being unable to find timelineCtrl.
    I don't know how to fix it.

--- www/js/controllers/timelineCtrl.js
+++ www/js/controllers/timelineCtrl.js
@@ -1,6 +1,6 @@
 angular.module('app.controllers')
 
-.controller('timelineCtrl', function($scope, $state, Card, CARD, MedicationSchedule, MeasurementSchedule) {
+.controller('timelineCtrl', function($scope, $state, Card, CARD, Comment, MedicationSchedule, MeasurementSchedule) {
   $scope.cards = Card.get();
   $scope.CARD = CARD;
 
@@ -30,26 +30,30 @@ angular.module('app.controllers')
   }
   $scope.generateCardsForToday = function() {
     var medSchedule = MedicationSchedule.get();
-    var measurementSchedule = MeasurementSchedule.get();
-    var today = new Date();
-    var currentDay = today.getDay();
+   var measurementSchedule = MeasurementSchedule.get();
+   var today = new Date();
+   var currentDay = today.getDay();
 
-    //Create medications Cards for the day
-    for (var i =0; i < medSchedule.length; i++) {
-       slot = medSchedule[i];
-       //console.log(slot.time);
+   //Create medications Cards for the day
+   for (var i =0; i < medSchedule.length; i++) {
+     slot = medSchedule[i];
+     //console.log(slot.time);
      if (slot.days.includes(currentDay)) {
-         Card.create_from_object(slot, CARD.CATEGORY.MEDICATIONS_SCHEDULE, CARD.TYPE.ACTION);
-      }
-    }
-
-    for(var i = 0; i < measurementSchedule.length; i++) {
-       slot = measurementSchedule[i];
-       if (slot.days.includes(currentDay)) {
-         Card.create_from_object(slot, CARD.CATEGORY.MEASUREMENTS_SCHEDULE, CARD.TYPE.ACTION);
-       }
+       Card.create_from_object(slot, CARD.CATEGORY.MEDICATIONS_SCHEDULE, CARD.TYPE.ACTION);
      }
-     console.log($scope.cards.length);
+   }
+
+   for(var i = 0; i < measurementSchedule.length; i++) {
+     slot = measurementSchedule[i];
+     if (slot.days.includes(currentDay)) {
+       Card.create_from_object(slot, CARD.CATEGORY.MEASUREMENTS_SCHEDULE, CARD.TYPE.ACTION);
+     }
+   }
+   console.log($scope.cards.length);
+  }
+
+  $scope.getCommentsCount = function(card_id){
+    return Comment.get_comments_count_by_id(card_id);
   }
 
   $scope.swipeCard = function(card) {
