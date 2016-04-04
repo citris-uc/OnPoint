814f73d4d874a25127ce5637eae200ce89ebe2a7
Bill Kim <bill.kim1007@gmail.com>
2016-04-04 14:33:46 -0700

    Commeneted our hardcoded schedule in MedicationSchedule factory

--- www/js/services/medication.js
+++ www/js/services/medication.js
@@ -48,33 +48,33 @@ angular.module('app.services')
 // This factory is responsible for defining a Medication Schedule
 // that the patient usually adheres to.
 .factory('MedicationSchedule', ["Medication", "Patient","$firebaseObject", "$firebaseArray", function(Medication, Patient, $firebaseObject,$firebaseArray) {
-  morning   = ["Lasix", "Toprol XL", "Zestril", "Coumadin", "Riomet"]
-  afternoon = ["Lasix", "Toprol XL", "Zestril", "Riomet"]
-  evening   = ["Lipitor"]
-
-  schedule = [
-    {
-      id: 1,
-      time: "08:00",
-      slot: "morning",
-      days: [0,1,2,3,4,5,6], //array descirbing days of week to do this action
-      medications: morning.map( function(trade_name) { return Medication.getByTradeName(trade_name) } )
-    },
-    {
-      id: 2,
-      time: "13:00",
-      slot: "afternoon",
-      days: [0,1,2,3,4,5,6], //array descirbing days of week to do this action,
-      medications: afternoon.map( function(trade_name) { return Medication.getByTradeName(trade_name) } )
-    },
-    {
-      id: 3,
-      time: "19:00",
-      slot: "evening",
-      days: [0,1,2,3,4,5,6], //array descirbing days of week to do this action,
-      medications: evening.map( function(trade_name) { return Medication.getByTradeName(trade_name) } )
-    }
-  ]
+  // morning   = ["Lasix", "Toprol XL", "Zestril", "Coumadin", "Riomet"]
+  // afternoon = ["Lasix", "Toprol XL", "Zestril", "Riomet"]
+  // evening   = ["Lipitor"]
+  //
+  // schedule = [
+  //   {
+  //     id: 1,
+  //     time: "08:00",
+  //     slot: "morning",
+  //     days: [0,1,2,3,4,5,6], //array descirbing days of week to do this action
+  //     medications: morning.map( function(trade_name) { return Medication.getByTradeName(trade_name) } )
+  //   },
+  //   {
+  //     id: 2,
+  //     time: "13:00",
+  //     slot: "afternoon",
+  //     days: [0,1,2,3,4,5,6], //array descirbing days of week to do this action,
+  //     medications: afternoon.map( function(trade_name) { return Medication.getByTradeName(trade_name) } )
+  //   },
+  //   {
+  //     id: 3,
+  //     time: "19:00",
+  //     slot: "evening",
+  //     days: [0,1,2,3,4,5,6], //array descirbing days of week to do this action,
+  //     medications: evening.map( function(trade_name) { return Medication.getByTradeName(trade_name) } )
+  //   }
+  // ]
 
   return {
 
