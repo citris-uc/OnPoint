ec826ab6a48371f843781161ee1956bcc677aba7
Bill Kim <bill.kim1007@gmail.com>
2016-04-04 13:33:27 -0700

    Commented out medicationSchedule references in generateCardsForToday
    -I did this to just focus on medicationsTab firebase integration

--- www/js/controllers/timelineCtrl.js
+++ www/js/controllers/timelineCtrl.js
@@ -29,19 +29,21 @@ angular.module('app.controllers')
     return false;
   }
   $scope.generateCardsForToday = function() {
-    var medSchedule = MedicationSchedule.get();
+    //COMMENTING OUT TO JUST FOCUS ON MEDICATIONS TAB FIREBASE INTEGRATION
+   //var medSchedule = MedicationSchedule.get();
    var measurementSchedule = MeasurementSchedule.get();
    var today = new Date();
    var currentDay = today.getDay();
 
+   //COMMENTING OUT TO JUST FOCUS ON MEDICATIONS TAB FIREBASE INTEGRATION
    //Create medications Cards for the day
-   for (var i =0; i < medSchedule.length; i++) {
-     slot = medSchedule[i];
-     //console.log(slot.time);
-     if (slot.days.includes(currentDay)) {
-       Card.create_from_object(slot, CARD.CATEGORY.MEDICATIONS_SCHEDULE, CARD.TYPE.ACTION);
-     }
-   }
+  //  for (var i =0; i < medSchedule.length; i++) {
+  //    slot = medSchedule[i];
+  //    //console.log(slot.time);
+  //    if (slot.days.includes(currentDay)) {
+  //      Card.create_from_object(slot, CARD.CATEGORY.MEDICATIONS_SCHEDULE, CARD.TYPE.ACTION);
+  //    }
+  //  }
 
    for(var i = 0; i < measurementSchedule.length; i++) {
      slot = measurementSchedule[i];
