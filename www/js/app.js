915588c0ea7d7ecc8e6ede1425721d4ffd167845
Bill Kim <bill.kim1007@gmail.com>
2016-04-10 15:46:36 -0700

    Changed app.js to allow users to go to register page

--- www/js/app.js
+++ www/js/app.js
@@ -25,7 +25,8 @@ angular.module('app', ['ionic','firebase', 'app.controllers', 'app.routes', 'app
   // state transition and go to the login screen.
   $rootScope.$on('$stateChangeStart', function (event, toState, toParams, fromState, fromParams, options) {
     var token = Patient.getToken();
-    if (!token && toState.name !== "loginScreen") {
+    if (!token && toState.name !== "loginScreen" && toState.name !== "registerScreen") {
+      //console.log(toState.name)
       event.preventDefault();
       $ionicHistory.nextViewOptions({
         disableAnimate: true,
