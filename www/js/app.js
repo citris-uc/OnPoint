9f645f3750b8ba03c02bd574d015b2b572c81e06
Angela H <angela@calvisitor-10-105-166-101.calvisitor.1918.berkeley.edu>
2016-04-12 18:08:31 -0700

    Fix whiteout issue by adding keyboard plugin to config.xml and fixing new_medication.html

--- www/js/app.js
+++ www/js/app.js
@@ -13,7 +13,9 @@ angular.module('app', ['ionic','firebase', 'app.controllers', 'app.routes', 'app
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
     if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      // Commented out due to https://github.com/driftyco/ionic-plugin-keyboard
+      // cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
     }
     if(window.StatusBar) {
       // org.apache.cordova.statusbar required
