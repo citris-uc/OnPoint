0bf2b7f32fee6420c77c4a9348eb6a411a126eff
zhuosi <wangzhuosi13@hotmail.com>
2016-04-18 18:38:14 -0700

    refactoring goals controller and goal service file.

---
+++ www/js/services/goal.js
@@ -0,0 +1,31 @@
+angular.module('app.services')
+
+// Factories allows us to define objects within our app. We can expose
+// specific methods within the object literal to mimic API calls, e.g.
+// Goal.get() returns all goals.
+.factory("Goal", ["Patient", "$firebaseArray", function(Patient, $firebaseArray) {
+  // TODO create enums for personal vs clinical
+
+  return {
+    get: function() {
+      var ref = this.ref();
+      return $firebaseArray(ref)
+    },
+    ref: function() {
+      var uid = Patient.uid();
+      return Patient.ref(uid).child("appointments")
+    },
+    add: function(goal) {
+      // Replace with Firebase
+      var g = {
+        body: goal.body,
+        type: goal.type
+      }
+
+      var goals = this.get();
+      var req = goals.$add(g);
+
+      return g;
+    }
+  };
+}])
