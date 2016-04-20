bae8d05710ce89ec76e0784a40fc0d253f68d437
Bill Kim <bill.kim1007@gmail.com>
2016-04-20 12:49:02 -0700

    Made medicatino cards self contained with timeline tab all the way thru

--- www/js/controllers/timelineCtrl.js
+++ www/js/controllers/timelineCtrl.js
@@ -387,7 +387,7 @@ angular.module('app.controllers')
         // Get schedule associated with card
         //var schedule = $scope.medSchedule[index]
         // action = {tab: 'tabsController.medicationsSchedule', params: {schedule_id: schedule.$id}};
-         action = {tab: 'tabsController.medicationAction', params: {schedule_id: schedule.$id}};
+         action = {tab: 'tabsController.medicationCardAction', params: {schedule_id: schedule.$id}};
         return $state.go(action.tab, action.params);
       case CARD.CATEGORY.MEASUREMENTS_SCHEDULE:
         var schedule = $scope.findMeasurementScheduleForCard(card)
