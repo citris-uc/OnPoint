ab1632952b601b8c021702147304bc8bc808569a
Dmitri Skjorshammer <dmitriskj@gmail.com>
2016-04-20 14:46:08 -0700

    Code cleanup.

--- www/js/services/card.js
+++ www/js/services/card.js
@@ -31,44 +31,10 @@ angular.module('app.services')
     },
 
     create: function(date, card) {
-      // DEPRECIATED: do NOT want to set the id of each card to be the object_id, want a unique_id and have child object_id!
-      // var ref = this.ref().child(date).child(object.type).child(object.id);
-      // ref.set(card);
-
       var ref = this.ref().child(date);
       ref.push(card); //use push to generate a UNIQUE card ID for each firebase card.
     },
-    // DEPRECIATED
-    // find_or_create_by_object(object, cardObject) {
-    //   console.log(object)
-    //   var ref = this.todaysRef().child(object.type).child(object.id)
-    //   return ref.transaction(function(current) {
-    //     if (!current)
-    //       current = {}
-    //
-    //     var now    = (new Date()).toISOString();
-    //     current.created_at   = now;
-    //     current.updated_at   = now;
-    //     current.shown_at     = cardObject.shown_at || (new Date()).toISOString();
-    //     current.completed_at = cardObject.completed_at || null;
-    //     current.archived_at  = null;
-    //     current.type         = cardObject.type
-    //     return current
-    //   })
-    // },
     complete: function(card) {
-      // var card;
-      // for(var i = 0; i < cards.length; i++) {
-      //   if (cards[i].id === cardID)
-      //     card = cards[i]
-      // }
-      //
-      // if (card) {
-      //   now = (new Date()).toISOString();
-      //   card.updated_at   = now;
-      //   card.completed_at = now;
-      // }
-      // return card;
       var ref = this.todaysRef().child(card.$id);
       var now = (new Date()).toTimeString();
 
@@ -86,19 +52,14 @@ angular.module('app.services')
       })
       return req;
     },
-
-
     archive: function(card) {
       var ref = this.todaysRef().child(card.$id);
       var now = (new Date()).toTimeString();
 
-      var updateObject; //use this if updating an element. see https://www.firebase.com/docs/web/api/firebase/update.html
-      updateObject = {updated_at: now,
-                      archived_at: now};
+      var updateObject = {updated_at: now, archived_at: now};
 
-      //Add to or update firebase
       var req = ref.once('value', function(snapshot) {
-        if(snapshot.exists()) { //this date child exists
+        if(snapshot.exists()) {
           var card = snapshot.val();
           var cardRef = snapshot.ref();
           cardRef.update(updateObject);
@@ -106,7 +67,5 @@ angular.module('app.services')
       })
       return req;
     }
-  };
-
-
+  }
 }])
