9a2532c2c2df30d6c5afb427aaacf00f33e6dc55
Bill Kim <bill.kim1007@gmail.com>
2016-05-06 13:34:28 -0700

    Clears cache on logout
    -so current acct doesnt interefere with next account

--- www/js/controllers/settingsCtrl.js
+++ www/js/controllers/settingsCtrl.js
@@ -3,6 +3,7 @@ angular.module('app.controllers')
 .controller('settingsCtrl', function($scope, Patient, $state, $ionicHistory) {
   $scope.logout = function() {
     Patient.logout();
+    $ionicHistory.clearCache(); //so current login does not interfere with next login
     $ionicHistory.nextViewOptions({
       disableAnimate: true,
       disableBack: true,
