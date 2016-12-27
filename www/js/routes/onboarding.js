a4fce18b2ec5397e5a0167c7d7b6287a6634a1c2
Dmitri Skjorshammer <dmitriskj@gmail.com>
2016-12-27 11:36:31 -0800

    Adds onboarding routes.

--- www/js/routes/onboarding.js
+++ www/js/routes/onboarding.js
@@ -1,6 +1,8 @@
 angular.module('app.routes')
 
 .config(function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/onboarding/welcome')
+
   // Ionic uses AngularUI Router which uses the concept of states
   // Learn more here: https://github.com/angular-ui/ui-router
   // Set up the various states which the app can be in.
@@ -8,7 +10,7 @@ angular.module('app.routes')
   $stateProvider
   .state('onboarding', {
     url: '/onboarding',
-    templateUrl: 'templates/onboarding.html',
+    templateUrl: 'templates/onboarding/menu.html',
     abstract: true
   })
   .state('onboarding.welcome', {
@@ -16,7 +18,39 @@ angular.module('app.routes')
     views: {
       'onboarding': {
         templateUrl: 'templates/onboarding/welcome.html',
-        controller: 'onboardingCtrl'
+      }
+    }
+  })
+  .state('onboarding.identify_meds', {
+    url: '/identify_meds',
+    views: {
+      'onboarding': {
+        templateUrl: 'templates/onboarding/identify_meds.html',
+      }
+    }
+  })
+  .state('onboarding.schedule_meds', {
+    url: '/schedule_meds',
+    views: {
+      'onboarding': {
+        templateUrl: 'templates/onboarding/schedule_meds.html',
+      }
+    }
+  })
+  .state('onboarding.timeline', {
+    url: '/timeline',
+    views: {
+      'onboarding': {
+        templateUrl: 'templates/onboarding/timeline.html',
+      }
+    }
+  })
+  .state('onboarding.finish', {
+    url: '/finish',
+    views: {
+      'onboarding': {
+        templateUrl: 'templates/onboarding/finish.html',
+        controller: "onboardingCtrl"
       }
     }
   })
