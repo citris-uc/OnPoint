f8606d12eaf4194b11a5fb5a340c4c5739936c18
Dmitri Skjorshammer <dmitriskj@gmail.com>
2016-12-27 10:21:51 -0800

    Adds compiled environment.js files.

---
+++ www/js/environment.js
@@ -0,0 +1,14 @@
+onpoint = {};
+onpoint.env = {
+  error: "error",
+  auth: {
+    success: "auth:success",
+    failure:   "auth:failure"
+  },
+  data: {
+    refresh: "data.refresh"
+  },
+  baseURL: "http://localhost:5000/api/v0/",
+  mainURL: "https://vivid-inferno-5187.firebaseio.com/",
+  debug: true
+}
