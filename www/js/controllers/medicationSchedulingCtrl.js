5201d943bbc965467aed34bfc6a1d35d7e5c9af2
Dmitri Skjorshammer <dmitriskj@gmail.com>
2016-12-27 18:20:56 -0800

    Replaces fillChoice with fill_pillbox.

--- www/js/controllers/medicationSchedulingCtrl.js
+++ www/js/controllers/medicationSchedulingCtrl.js
@@ -68,14 +68,13 @@ angular.module('app.controllers')
     //TODO: when editing new schedule need to createa  new schedule in FB, set obj_id to the old schedule id or new one?
     var oldScheduleRef = 'default';
     Card.createAdHoc(CARD.CATEGORY.MEDICATIONS_SCHEDULE_CHANGE, oldScheduleRef, (new Date()).toISOString())
-    if($ionicHistory.currentStateName() == 'medication_scheduling.start') {
-      //Done onboarding!
-      var ref = Patient.ref();
-      var req = ref.child('onboarding').update({'completed':true,'state':$state.current.name})
-      $state.go("carePlan.fillChoice");
-    } else if ($ionicHistory.currentStateName() == 'tabsController.editMedSchedule'){
-      $state.go("tabsController.medications");
-    }
+
+    //Done onboarding!
+    var ref = Patient.ref();
+    var req = ref.child('medication_scheduling').update({'completed':true})
+    $state.go("medication_scheduling.fill_pillbox");
+
+
   }
 })
 
@@ -255,6 +254,78 @@ angular.module('app.controllers')
     }
     // Default Image
     return "img/pill_small.png";
+  }
+
+})
+
+
+.controller('medFillMainCtrl', function($scope, $state, $ionicHistory, MedicationSchedule, Medication, MedicationDosage) {
+  $scope.medicationSchedule = MedicationSchedule.get();
+  $scope.medications = Medication.get();
+  $scope.selectedMed;
+  var emptySlots = [' ',' ',' ',' ',' ',' ',' '];
+  var selectedMed = [];
+  $scope.getSlots = function(schedule, med) {
+    var DAYS_OF_THE_WEEK = 7
+    var slots = [];
+    if (typeof(med) === 'undefined') { //has not selected a med yet
+      slots = emptySlots
+    }
+    else {
+      if(schedule.medications.indexOf(med.trade_name) != -1) {
+        for(var day = 0; day < 7; day++) {
+          if (schedule.days[day]) {
+            slots.push(med.tablets);
+          } else {
+            slots.push(" ");
+          }
+        }
+      }
+      else {
+        slots = emptySlots //this med is not in this schedule slot
+      }
+    }
+    return slots
+  }
+  $scope.displaySchedule = function(med){
+    selectedMed.push(med);
+    $scope.selectedMed = med //set the selected med
+  }
+
+  $scope.currentlyOn = function(med){
+    if($scope.selectedMed == med){
+      return true;
+    }
+    return false;
+  }
+
+  $scope.hasSelected = function(med){
+    if(selectedMed.indexOf(med) != -1){
+      return true;
+    }
+    return false;
+  }
+
+  $scope.buttonStyle = function(med){
+    var selected = "background-color: gray";
+    var notSelected = "background-color: white";
+    var currentlyOn = "background-color: green";
+    if($scope.currentlyOn(med)){
+      return currentlyOn;
+    }
+    if($scope.hasSelected(med)){
+      return selected;
+    }
+    return notSelected;
+  }
+
+  $scope.doneMedSetup =  function() {
+    $ionicHistory.nextViewOptions({
+      disableAnimate: true,
+      disableBack: true,
+      historyRoot: true
+    })
+    $state.go('carePlan.setup');
   }
 
 })
