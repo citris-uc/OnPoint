dd7236d81cff11ebeddf0a7ad0b5935154a6b572
Dmitri Skjorshammer <dmitriskj@gmail.com>
2017-01-05 16:44:41 -0800

    Adds timelineCtrl and medications controller + updates routes.

--- www/js/controllers/medicationsCtrl.js
+++ www/js/controllers/medicationsCtrl.js
@@ -141,103 +141,103 @@ angular.module('app.controllers')
   }
 
 })
-
-.controller("medicationScheduleCtrl", function($scope, $state, $stateParams, $ionicHistory, Medication, MedicationSchedule, MedicationDosage, MedicationHistory) {
-  $scope.schedule = MedicationSchedule.findByID($stateParams.schedule_id);
-  $scope.medicationHistory  = MedicationHistory.getTodaysHistory();
-  $scope.medications        = Medication.get();
-
-  $scope.didTakeMed = function(medication) {
-    var match;
-    var med = {}
-    //Find the Med
-    for(var i = 0; i < $scope.medications.length; i++) {
-      if ($scope.medications[i].trade_name == medication) {
-        med.id = $scope.medications[i].$id;
-      }
-    }
-
-    //then find the history instance.
-    for(var i = 0; i < $scope.medicationHistory.length; i++) {
-      if ($scope.medicationHistory[i].medication_id == med.id && $scope.medicationHistory[i].medication_schedule_id == $stateParams.schedule_id) {
-        match = $scope.medicationHistory[i]
-      }
-    }
-
-    if (match)
-      return (match.taken_at !== undefined);
-    else
-      return false;
-  }
-
-  $scope.didSkipMed = function(medication) {
-    var match;
-    var med = {}
-    //Find the Med
-    for(var i = 0; i < $scope.medications.length; i++) {
-      if ($scope.medications[i].trade_name == medication) {
-        med.id = $scope.medications[i].$id;
-      }
-    }
-
-    //then find the history instance.
-    for(var i = 0; i < $scope.medicationHistory.length; i++) {
-      if ($scope.medicationHistory[i].medication_id == med.id && $scope.medicationHistory[i].medication_schedule_id == $stateParams.schedule_id) {
-        match = $scope.medicationHistory[i]
-      }
-    }
-
-    if (match)
-      return (match.skipped_at !== undefined);
-    else
-      return false;
-  }
-
-  /*
-   * Can get to templates/medications/schedule.html in 2 ways so need to direct approrpiately when click a specific med
-   * rather than having a hard coded ui-sref like below
-   * ui-sref="tabsController.medicationAction({schedule_id: schedule.$id, medication_name: med})"
-   * ui-sref="tabsController.medication({schedule_id: schedule.$id, medication_name: med})"
-   */
-  $scope.directToMed = function(schedule, med_name) {
-    var params =  {schedule_id: schedule, medication_name: med_name};
-
-    if($ionicHistory.backView().stateName=='tabsController.timeline')
-      $state.go('tabsController.medicationAction',params)
-    else if ($ionicHistory.backView().stateName=='tabsController.medications')
-      $state.go('tabsController.medication',params)
-
-  }
-
-  $scope.takeAll = function(){
-    for(var i = 0; i < $scope.schedule.medications.length; i++){
-      if(this.didTakeMed($scope.schedule.medications[i]) == false){
-        var req = Medication.getByTradeName($scope.schedule.medications[i])
-        req.then(function(snapshot) {
-          $scope.medication = snapshot.val()
-          $scope.medication["id"] =  snapshot.key()
-           var req = MedicationHistory.create_or_update($scope.medication, $scope.schedule, "take");
-        })
-      }
-    }
-    $ionicHistory.goBack();
-  }
-
-  $scope.goBack = function(){
-    $ionicHistory.goBack();
-  }
-  $scope.containMeds = function(){
-    if( typeof $scope.schedule.medications === "undefined"){
-      return false;
-    }
-    for(var i = 0; i < $scope.schedule.medications.length; i++ ) {
-      if(this.didTakeMed($scope.schedule.medications[i]) == false){
-        return true;
-      }
-    }
-    return false;
-  }
-})
+// 
+// .controller("medicationScheduleCtrl", function($scope, $state, $stateParams, $ionicHistory, Medication, MedicationSchedule, MedicationDosage, MedicationHistory) {
+//   $scope.schedule = MedicationSchedule.findByID($stateParams.schedule_id);
+//   $scope.medicationHistory  = MedicationHistory.getTodaysHistory();
+//   $scope.medications        = Medication.get();
+//
+//   $scope.didTakeMed = function(medication) {
+//     var match;
+//     var med = {}
+//     //Find the Med
+//     for(var i = 0; i < $scope.medications.length; i++) {
+//       if ($scope.medications[i].trade_name == medication) {
+//         med.id = $scope.medications[i].$id;
+//       }
+//     }
+//
+//     //then find the history instance.
+//     for(var i = 0; i < $scope.medicationHistory.length; i++) {
+//       if ($scope.medicationHistory[i].medication_id == med.id && $scope.medicationHistory[i].medication_schedule_id == $stateParams.schedule_id) {
+//         match = $scope.medicationHistory[i]
+//       }
+//     }
+//
+//     if (match)
+//       return (match.taken_at !== undefined);
+//     else
+//       return false;
+//   }
+//
+//   $scope.didSkipMed = function(medication) {
+//     var match;
+//     var med = {}
+//     //Find the Med
+//     for(var i = 0; i < $scope.medications.length; i++) {
+//       if ($scope.medications[i].trade_name == medication) {
+//         med.id = $scope.medications[i].$id;
+//       }
+//     }
+//
+//     //then find the history instance.
+//     for(var i = 0; i < $scope.medicationHistory.length; i++) {
+//       if ($scope.medicationHistory[i].medication_id == med.id && $scope.medicationHistory[i].medication_schedule_id == $stateParams.schedule_id) {
+//         match = $scope.medicationHistory[i]
+//       }
+//     }
+//
+//     if (match)
+//       return (match.skipped_at !== undefined);
+//     else
+//       return false;
+//   }
+//
+//   /*
+//    * Can get to templates/medications/schedule.html in 2 ways so need to direct approrpiately when click a specific med
+//    * rather than having a hard coded ui-sref like below
+//    * ui-sref="tabsController.medicationAction({schedule_id: schedule.$id, medication_name: med})"
+//    * ui-sref="tabsController.medication({schedule_id: schedule.$id, medication_name: med})"
+//    */
+//   $scope.directToMed = function(schedule, med_name) {
+//     var params =  {schedule_id: schedule, medication_name: med_name};
+//
+//     if($ionicHistory.backView().stateName=='tabsController.timeline')
+//       $state.go('tabsController.medicationAction',params)
+//     else if ($ionicHistory.backView().stateName=='tabsController.medications')
+//       $state.go('tabsController.medication',params)
+//
+//   }
+//
+//   $scope.takeAll = function(){
+//     for(var i = 0; i < $scope.schedule.medications.length; i++){
+//       if(this.didTakeMed($scope.schedule.medications[i]) == false){
+//         var req = Medication.getByTradeName($scope.schedule.medications[i])
+//         req.then(function(snapshot) {
+//           $scope.medication = snapshot.val()
+//           $scope.medication["id"] =  snapshot.key()
+//            var req = MedicationHistory.create_or_update($scope.medication, $scope.schedule, "take");
+//         })
+//       }
+//     }
+//     $ionicHistory.goBack();
+//   }
+//
+//   $scope.goBack = function(){
+//     $ionicHistory.goBack();
+//   }
+//   $scope.containMeds = function(){
+//     if( typeof $scope.schedule.medications === "undefined"){
+//       return false;
+//     }
+//     for(var i = 0; i < $scope.schedule.medications.length; i++ ) {
+//       if(this.didTakeMed($scope.schedule.medications[i]) == false){
+//         return true;
+//       }
+//     }
+//     return false;
+//   }
+// })
 
 .controller("medicationCtrl", function($scope, $stateParams,$ionicPopup,$ionicHistory, Medication, MedicationSchedule, MedicationDosage, MedicationHistory) {
   $scope.state = $stateParams;
--- www/js/routes.js
+++ www/js/routes.js
@@ -27,57 +27,6 @@ angular.module('app.routes', [])
     abstract:true
   })
 
-  .state('tabsController.timeline', {
-    url: '/timeline',
-    views: {
-      'timeline-tab': {
-        templateUrl: 'templates/timeline.html',
-        controller: 'timelineCtrl'
-      }
-    }
-  })
-
-  .state('tabsController.medicationCardAction', {
-    url: '/timeline/medication-card-action/:schedule_id',
-    views: {
-      'timeline-tab': {
-        templateUrl: 'templates/medications/schedule.html',
-        controller: 'medicationScheduleCtrl'
-      }
-    }
-  })
-
-  .state('tabsController.medicationAction', {
-    url: '/timeline/medication-card-action/:schedule_id/medication-action/:medication_name',
-    views: {
-      'timeline-tab': {
-        templateUrl: 'templates/medications/show.html',
-        controller: 'medicationCtrl'
-      }
-    }
-  })
-
-  .state('tabsController.measurementAction', {
-    url: '/timeline/measurement-action/:schedule_id',
-    views: {
-      'timeline-tab': {
-        templateUrl: 'templates/measurements/measurements-add.html',
-        controller: 'addMeasurementsCtrl'
-      }
-    }
-  })
-
-  .state('tabsController.measurementActionTips', {
-    url: '/measurement-tips',
-    views: {
-      'timeline-tab': {
-        templateUrl: 'templates/measurements/measurementTips.html',
-        controller: 'measurementTipsCtrl'
-      }
-    }
-  })
-
-
   .state('tabsController.settings', {
     url: '/settings',
     views: {
@@ -88,36 +37,39 @@ angular.module('app.routes', [])
     }
   })
 
-  .state('tabsController.notes', {
-    url: '/settings/notes',
+  .state('tabsController.timeline', {
+    url: '/timeline',
     views: {
       'timeline-tab': {
-        templateUrl: 'templates/notes.html',
-        controller: 'notesCtrl'
+        templateUrl: 'templates/timeline.html',
+        controller: 'timelineCtrl'
       }
     }
   })
 
-  .state('tabsController.medications', {
-    url: '/medications',
+  .state('tabsController.medication_schedule', {
+    url: '/timeline/medication_schedule/:schedule_id',
     views: {
-      'medications-tab': {
-        templateUrl: 'templates/medications/medications.html',
-        controller: 'medicationsCtrl'
+      'timeline-tab': {
+        templateUrl: 'templates/timeline/medication_schedule.html',
+        controller: 'medicationScheduleCardCtrl'
       }
     }
   })
 
-  .state('tabsController.medicationsSchedule', {
-    url: '/medications/:schedule_id',
+  .state('tabsController.medication_schedule.medication', {
+    url: '/medication/:medication_name',
     views: {
-      'medications-tab': {
-        templateUrl: 'templates/medications/schedule.html',
-        controller: 'medicationScheduleCtrl'
+      'timeline-tab@tabsController': {
+        templateUrl: 'templates/timeline/medication.html',
+        controller: 'medicationScheduleCardCtrl'
       }
     }
   })
 
+
+
+  // TODO: Deprecate these
   .state('tabsController.medication', {
     url: '/medications/schedule/:schedule_id/medication/:medication_name',
     views: {
@@ -158,6 +110,58 @@ angular.module('app.routes', [])
     }
   })
 
+
+  // .state('tabsController.measurementAction', {
+  //   url: '/timeline/measurement-action/:schedule_id',
+  //   views: {
+  //     'timeline-tab': {
+  //       templateUrl: 'templates/measurements/measurements-add.html',
+  //       controller: 'addMeasurementsCtrl'
+  //     }
+  //   }
+  // })
+  //
+  // .state('tabsController.measurementActionTips', {
+  //   url: '/measurement-tips',
+  //   views: {
+  //     'timeline-tab': {
+  //       templateUrl: 'templates/measurements/measurementTips.html',
+  //       controller: 'measurementTipsCtrl'
+  //     }
+  //   }
+  // })
+
+
+  .state('tabsController.notes', {
+    url: '/settings/notes',
+    views: {
+      'timeline-tab': {
+        templateUrl: 'templates/notes.html',
+        controller: 'notesCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.medications', {
+    url: '/medications',
+    views: {
+      'medications-tab': {
+        templateUrl: 'templates/medications/medications.html',
+        controller: 'medicationsCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.medicationsSchedule', {
+    url: '/medications/:schedule_id',
+    views: {
+      'medications-tab': {
+        templateUrl: 'templates/medications/schedule.html',
+        controller: 'medicationScheduleCtrl'
+      }
+    }
+  })
+
   .state('tabsController.editMedSchedule', {
     url: '/medication_schedules/generated',
     views: {
@@ -178,26 +182,36 @@ angular.module('app.routes', [])
     }
   })
 
-
-  .state('tabsController.measurements', {
-    url: '/measurements',
+  .state('tabsController.comments', {
+    url: '/cards/:card_id/comments/:comment_id',
     views: {
-      'measurements-tab': {
-        templateUrl: 'templates/measurements/measurements.html',
-        controller: 'measurementsCtrl'
+      'timeline-tab': {
+        templateUrl: 'templates/comments.html',
+        controller: 'commentsCtrl'
       }
     }
   })
 
-  .state('tabsController.viewMeasurementSchedule', {
-    url: 'measurements/schedule/:measurement_schedule_id',
-    views: {
-      'measurements-tab': {
-        templateUrl: 'templates/measurements/view_measurement_schedule.html',
-        controller: 'measurementViewCtrl'
-      }
-    }
-  })
+
+  // .state('tabsController.measurements', {
+  //   url: '/measurements',
+  //   views: {
+  //     'measurements-tab': {
+  //       templateUrl: 'templates/measurements/measurements.html',
+  //       controller: 'measurementsCtrl'
+  //     }
+  //   }
+  // })
+  //
+  // .state('tabsController.viewMeasurementSchedule', {
+  //   url: 'measurements/schedule/:measurement_schedule_id',
+  //   views: {
+  //     'measurements-tab': {
+  //       templateUrl: 'templates/measurements/view_measurement_schedule.html',
+  //       controller: 'measurementViewCtrl'
+  //     }
+  //   }
+  // })
 
   // DEPRECIATED: Making measurements tab a segmenetd control
   // .state('tabsController.measurementAdd', {
@@ -220,85 +234,75 @@ angular.module('app.routes', [])
   //   }
   // })
 
-  .state('tabsController.appointments', {
-    url: '/appointments',
-    views: {
-      'appointments-tab': {
-        templateUrl: 'templates/appointments/appointments.html',
-        controller: 'appointmentsCtrl'
-      }
-    }
-  })
+  // .state('tabsController.appointments', {
+  //   url: '/appointments',
+  //   views: {
+  //     'appointments-tab': {
+  //       templateUrl: 'templates/appointments/appointments.html',
+  //       controller: 'appointmentsCtrl'
+  //     }
+  //   }
+  // })
+  //
+  // .state('tabsController.appointment', {
+  //   url: '/appointment/:date/:appointment_id',
+  //   views: {
+  //     'timeline-tab': {
+  //       templateUrl: 'templates/appointments/appointment.html',
+  //       controller: 'appointmentCtrl'
+  //     }
+  //   }
+  // })
+  //
+  // .state('tabsController.addAppointment', {
+  //   url: '/add-appointment',
+  //   views: {
+  //     'appointments-tab': {
+  //       templateUrl: 'templates/appointments/addAppointment.html',
+  //       controller: 'addAppointmentCtrl'
+  //     }
+  //   }
+  // })
 
-  .state('tabsController.appointment', {
-    url: '/appointment/:date/:appointment_id',
-    views: {
-      'timeline-tab': {
-        templateUrl: 'templates/appointments/appointment.html',
-        controller: 'appointmentCtrl'
-      }
-    }
-  })
+  // .state('tabsController.editAppointment', {
+  //   url: '/edit-appointment/:date/:appointment_id',
+  //   views: {
+  //     'appointments-tab': {
+  //       templateUrl: 'templates/appointments/editAppointment.html',
+  //       controller: 'editAppointmentCtrl'
+  //     }
+  //   }
+  // })
 
-  .state('tabsController.addAppointment', {
-    url: '/add-appointment',
-    views: {
-      'appointments-tab': {
-        templateUrl: 'templates/appointments/addAppointment.html',
-        controller: 'addAppointmentCtrl'
-      }
-    }
-  })
-
-  .state('tabsController.editAppointment', {
-    url: '/edit-appointment/:date/:appointment_id',
-    views: {
-      'appointments-tab': {
-        templateUrl: 'templates/appointments/editAppointment.html',
-        controller: 'editAppointmentCtrl'
-      }
-    }
-  })
-
-  .state('tabsController.comments', {
-    url: '/cards/:card_id/comments/:comment_id',
-    views: {
-      'timeline-tab': {
-        templateUrl: 'templates/comments.html',
-        controller: 'commentsCtrl'
-      }
-    }
-  })
-
-  .state('tabsController.goals', {
-    url: '/goals',
-    views: {
-      'goals-tab': {
-        templateUrl: 'templates/goals.html',
-        controller: 'goalsCtrl'
-      }
-    }
-  })
-
-  .state('tabsController.addGoal', {
-    url: '/add-goal',
-    views: {
-      'goals-tab': {
-        templateUrl: 'templates/addGoal.html',
-        controller: 'goalsCtrl'
-      }
-    }
-  })
-
-  .state('tabsController.editGoal', {
-    url: '/goal/:goal_id',
-    views: {
-      'goals-tab': {
-        templateUrl: 'templates/editGoal.html',
-        controller: 'editGoalCtrl'
-      }
-    }
-  })
+  // .state('tabsController.goals', {
+  //   url: '/goals',
+  //   views: {
+  //     'goals-tab': {
+  //       templateUrl: 'templates/goals.html',
+  //       controller: 'goalsCtrl'
+  //     }
+  //   }
+  // })
+  //
+  // .state('tabsController.addGoal', {
+  //   url: '/add-goal',
+  //   views: {
+  //     'goals-tab': {
+  //       templateUrl: 'templates/addGoal.html',
+  //       controller: 'goalsCtrl'
+  //     }
+  //   }
+  // })
+  //
+  // .state('tabsController.editGoal', {
+  //   url: '/goal/:goal_id',
+  //   views: {
+  //     'goals-tab': {
+  //       templateUrl: 'templates/editGoal.html',
+  //       controller: 'editGoalCtrl'
+  //     }
+  //   }
+  // })
 
   .state('symptomsSlider', {
     url: '/tabs4',
