a5d61dfda4be308bd266139d7a1ca847769a83de
Dmitri Skjorshammer <dmitriskj@gmail.com>
2017-01-25 22:34:03 -0800

    Replaces all onboarding views with a single ion-slide-box.

--- www/js/controllers/onboardingCtrl.js
+++ www/js/controllers/onboardingCtrl.js
@@ -1,17 +1,26 @@
 angular.module('app.controllers')
-.controller('onboardingCtrl', function($scope, $state, Patient) {
-  $scope.patient = Patient.get()
-  console.log($scope.patient)
-
+.controller('onboardingCtrl', function($scope, $state, Patient, $ionicSlideBoxDelegate) {
   $scope.logout = function() {
     Patient.logout()
   }
+
+  $scope.next = function() {
+     $ionicSlideBoxDelegate.next();
+   };
+   $scope.previous = function() {
+     $ionicSlideBoxDelegate.previous();
+   };
+
+   // Called each time the slide changes
+   $scope.slideChanged = function(index) {
+     $scope.slideIndex = index;
+   };
 
   $scope.completeOnboarding = function() {
     var onboardingRef = Patient.ref().child('onboarding');
     onboardingRef.set({'intro':true}).then(function(response) {
       Patient.setAttribute("onboarding", {"intro": true})
-      $state.go("medication_identification.welcome")
+      $state.go("medication_identification.start")
     })
   }
 })
--- www/js/routes/onboarding.js
+++ www/js/routes/onboarding.js
@@ -21,40 +21,6 @@ angular.module('app.routes')
       }
     }
   })
-  .state('onboarding.identify_meds', {
-    url: '/identify_meds',
-    views: {
-      'onboarding': {
-        templateUrl: 'templates/onboarding/identify_meds.html',
-      }
-    }
-  })
-  .state('onboarding.schedule_meds', {
-    url: '/schedule_meds',
-    views: {
-      'onboarding': {
-        templateUrl: 'templates/onboarding/schedule_meds.html',
-      }
-    }
-  })
-  .state('onboarding.timeline', {
-    url: '/timeline',
-    views: {
-      'onboarding': {
-        templateUrl: 'templates/onboarding/timeline.html',
-        controller: "onboardingCtrl"
-      }
-    }
-  })
-  .state('onboarding.ready_to_start', {
-    url: '/finish',
-    views: {
-      'onboarding': {
-        templateUrl: 'templates/onboarding/ready_to_start.html',
-        controller: "onboardingCtrl"
-      }
-    }
-  })
   .state('onboarding.complete', {
     url: '/complete',
     views: {
