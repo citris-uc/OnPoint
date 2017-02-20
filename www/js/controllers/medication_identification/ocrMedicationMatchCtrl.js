c5765568c73ef2eda8effab29b669aa6979a487d
Dmitri Skjorshammer <dmitriskj@gmail.com>
2017-02-20 13:37:39 -0800

    Adds ocrMedicationMatchCtrl.

---
+++ www/js/controllers/medication_identification/ocrMedicationMatchCtrl.js
@@ -0,0 +1,34 @@
+
+angular.module('app.controllers')
+.controller('ocrMedicationMatchCtrl', function($scope, $state, $ionicPopover, Medication, $ionicLoading, $ionicHistory) {
+  $scope.drug       = {}
+  $scope.medication = {}
+  $scope.ocr        = angular.fromJson($state.params.ocr)
+
+  $scope.navigateToMatch = function(drug) {
+    // ui-sref="medication_identification.match({rxcui: drug.rxcui})"
+    $state.go("medication_identification.match", {rxcui: drug.rxcui, ocr: $state.params.ocr}, {reload: true})
+  }
+
+  $scope.$on("$ionicView.loaded", function() {
+    // alert($state.params.ocr)
+    $ionicLoading.show({hideOnStateChange: true})
+
+    if (onpoint.env.environment == "development")
+      $scope.ocr = {drug_name: "LAMOTRIGINE", amount: "2 TABLETS", delivery: "BY MOUTH", frequency: "EVERY DAY"}
+
+    Medication.search($scope.ocr.drug_name).then(function(response) {
+      console.log(response)
+      $scope.drugs = response.data
+    }).catch(function(res) {
+      console.log("ERROR")
+      console.log(res)
+    }).finally(function(res) {
+      $ionicLoading.hide()
+    })
+  })
+
+  // $scope.add = function() {
+  //   $ionicHistory.goBack(-2)
+  // }
+})
