230dfcc46546275522595693f4c9e07b444956bc
Dmitri Skjorshammer <dmitriskj@gmail.com>
2017-02-20 14:29:13 -0800

    Display to edit medication.

--- www/js/controllers/medication_identification/medicationViewCtrl.js
+++ www/js/controllers/medication_identification/medicationViewCtrl.js
@@ -1,17 +1,27 @@
 angular.module('app.controllers')
 
 .controller('medicationViewCtrl', function($scope, $state, Medication, $ionicHistory, $ionicLoading) {
-  Medication.getById($state.params.id).then(function(med) {
-    $scope.medication = med
+  $scope.drug  = {}
+  $scope.units = [{value: "mg", display: "mg"}]
+
+  $scope.$on("$ionicView.loaded", function() {
+    $ionicLoading.show({hideOnStateChange: true});
+
+    Medication.getById($state.params.id).then(function(med) {
+      $scope.drug = med
+      console.log(med)
+    }).finally(function() {
+      $ionicLoading.hide();
+    })
   })
 
   $scope.update = function() {
     $ionicLoading.show({hideOnStateChange: true})
 
-    $scope.medication.$save().then(function(res) {
-      return $ionicLoading.hide()
-    }).then(function() {
+    $scope.drug.$save().then(function() {
       $ionicHistory.goBack(-1)
+    }).finally(function() {
+      return $ionicLoading.hide()
     })
   }
 
