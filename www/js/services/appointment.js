fe5c5aba2d12b0cbc901bc2bd7a77a34e52be7ae
Dmitri Skjorshammer <dmitriskj@gmail.com>
2017-02-27 11:37:18 -0800

    Refactors appointmentsCtrl and Appointment service.

--- www/js/services/appointment.js
+++ www/js/services/appointment.js
@@ -1,5 +1,5 @@
 angular.module('app.services')
-.factory('Appointment',["Patient","$firebaseObject", "$firebaseArray", function(Patient, $firebaseObject,$firebaseArray) {
+.factory('Appointment',["Patient", "$firebaseObject", "$firebaseArray", "moment", function(Patient, $firebaseObject,$firebaseArray, moment) {
   // var appointments = [{
   //   id: 1,
   //   timestamp: "2016-03-05T10:00:00",
@@ -21,14 +21,10 @@ angular.module('app.services')
   // }];
 
   return {
-    ref: function() {
-      var uid = Patient.uid();
-      return Patient.ref(uid).child("appointments")
-    },
-    get: function() {
-      var ref = this.ref();
-      return $firebaseArray(ref);
-      //return medications;
+    getAll: function() {
+      return Patient.get().then(function(p) {
+        return $firebaseArray(Patient.ref(p.uid).child("appointments")).$loaded()
+      })
     },
     /*
      * Return Appointments in firebase from dates, from, until dates, to
@@ -36,35 +32,63 @@ angular.module('app.services')
      * @param to: to date in javascript date format
      * @return: returns a firebaseArray of appointments
      */
-    getAppointmentsFromTo: function(from, to) {
-      var ref = this.getAppointmentsFromToRef(from,to)
-      return $firebaseArray(ref);
-    },
-    getAppointmentsFromToRef: function(from, to) {
-      var fromISO = from.toISOString().substring(0,10);
-      var toISO = to.toISOString().substring(0,10);
-      var ref = this.ref().orderByKey().startAt(fromISO).endAt(toISO);
-      return ref;
-    },
+    // getAppointmentsFromTo: function(from, to) {
+    //   var ref = this.getAppointmentsFromToRef(from,to)
+    //   return $firebaseArray(ref);
+    // },
+    // getAppointmentsFromToRef: function(from, to) {
+    //   var fromISO = from.toISOString().substring(0,10);
+    //   var toISO = to.toISOString().substring(0,10);
+    //   var ref = this.ref().orderByKey().startAt(fromISO).endAt(toISO);
+    //   return ref;
+    // },
 
-    getById: function(date,appointment_id){
-      return $firebaseObject(this.ref().child(date).child(appointment_id));
+    get: function(date, id){
+      return Patient.get().then(function(p) {
+        return $firebaseObject(Patient.ref(p.uid).child("appointments").child(date).child(appointment_id));
+      })
     },
     add: function(appointment) {
-      var apptDate = (appointment.time.toISOString()).substring(0,10);
-      var ref = this.ref().child(apptDate);
-      var a = {
-        time: appointment.time.toISOString(),
-        title: appointment.title,
-        location: appointment.location,
-        note: appointment.note
-      }
-      ref.push(a);
+      appt = {}
+      for (var key in appointment)
+        appt[key] = appointment[key]
+      appt.date = moment(appointment.date).format('YYYY-MM-DD');
+      appt.time = moment(appointment.time).format('HH:mm');
+      return Patient.get().then(function(p) {
+        return Patient.ref(p.uid).child("appointments").child(appt.date).push(appt);
+      });
     },
-    update: function(appointment, dateKey, apptKey) {
-      var ref = this.ref().child(dateKey).child(apptKey);
-      console.log(appointment)
-      ref.update(appointment);
+    update: function(oldDate, oldKey, appointment) {
+      console.log("Updating...")
+      console.log(oldDate)
+      console.log(oldKey)
+      console.log(appointment.date)
+      console.log(appointment.time)
+      console.log("--------------")
+
+      thisAppt  = this
+      appt = {}
+      for (var key in appointment)
+        appt[key] = appointment[key]
+      appt.date = moment(appointment.date).format('YYYY-MM-DD');
+      appt.time = moment(appointment.time).format('HH:mm');
+
+      console.log("Dates converted: ")
+
+      puid = null
+      return Patient.get().then(function(p) {
+        puid = p.uid
+        return $firebaseObject(Patient.ref(puid).child("appointments").child(oldDate).child(oldKey)).$remove()
+      }).then(function(oldAppt) {
+        return Patient.get().then(function(p) {
+          return Patient.ref(p.uid).child("appointments").child(appt.date).push(appt);
+        });
+      })
+    },
+    destroy: function(appt_date, appt_id) {
+      return Patient.get().then(function(p) {
+        return $firebaseObject(Patient.ref(p.uid).child("appointments").child(appt_date).child(appt_id)).$remove()
+      })
     }
   };
 }])
